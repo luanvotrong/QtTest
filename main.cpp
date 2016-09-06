e4a75dc796752b5da74392548d148b4f62ec8bdd
luan.votrong <vtluan288@gmail.com>
2016-09-06 22:45:01 +0700

    - Update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QtWebEngine>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+
+    QtWebEngine::initialize();
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
--- main.qml
+++ main.qml
@@ -1,8 +1,14 @@
 import QtQuick 2.7
 import QtQuick.Window 2.2
+import QtWebEngine 1.3
 
 Window {
     visible: true
     visibility: "Maximized"
     title: qsTr("Hello World")
+
+    WebEngineView{
+        anchors.fill: parent
+        url: "http://www.google.com"
+    }
 }
